78b5eb4b756e7fb603de4db7ba32c2ebb476fd91
OMEN-DELOBELLE-\orgel <remi.delobelle01@gmail.com>
2023-05-10 13:46:43 +0200

    Clear knop toegevoegd, werkt helemaal Save knop toegevoegd, slaat op in .JSON, werkt helemaal Choose en Load knop toegevoegd, nog werken aan exception handling

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
-
 using NAudio.SoundFont;
 using NAudio.Wave;
 using System.Threading;
@@ -16,6 +15,11 @@ using System.Media;
 using System.Linq;
 //using DrumMachine;
 using System.Windows.Threading;
+using Microsoft.Win32;
+using System.Text.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+//using Newtonsoft.Json;
 
 namespace DrumMachine_Project_OOP
 {
@@ -39,6 +43,9 @@ namespace DrumMachine_Project_OOP
         int _i = 0;
         DispatcherTimer _dispatcherTimer;
 
+        List<Button> soundBtnList = new List<Button>();
+        List<Button> antiBtnList = new List<Button>();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -46,6 +53,18 @@ namespace DrumMachine_Project_OOP
             _dispatcherTimer = new DispatcherTimer(DispatcherPriority.Normal);
             _dispatcherTimer.Interval = TimeSpan.FromMilliseconds(msWait);
             _dispatcherTimer.Tick += _dispatcherTimer_Tick;
+
+            antiBtnList.AddRange(new[]
+            {
+                btnChoose,
+                btnLoad,
+                btnPlay,
+                btnStop,
+                btnClear,
+                btnSave,                
+            });
+            foreach (Button item in antiBtnList)
+            {   Debug.WriteLine(item.Name);     }
         }
 
         private void _dispatcherTimer_Tick(object? sender, EventArgs e)
@@ -67,142 +86,151 @@ namespace DrumMachine_Project_OOP
                 _i = 0;   
         }
 
-        private void Window_KeyDown(object sender, KeyEventArgs e)
+        //private void Window_KeyDown(object sender, KeyEventArgs e)
+        //{
+
+        //}
+
+        private void btnChoose_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "JSON Files (*.json)|*.json";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                txtBxFilePath.Text = openFileDialog.FileName;
+            }
         }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+            string filePath = txtBxFilePath.Text;
+
+            try
+            {
+                string fileContent = File.ReadAllText(filePath);
+                SaveArrays(filePath);
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show("The selected file does not exist.", "File Not Found", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while processing the file: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void SaveArrays(string filePath)
+        {
+            string json = File.ReadAllText(filePath);
+
+            JArray jsonArray = JArray.Parse(json);
+
+            // Extract the arrays
+            try
+            {
+                crashMem = jsonArray[0]?.ToObject<int[]>() ?? throw NullArrayExc(nameof(crashMem));
+                hihatMem = jsonArray[1]?.ToObject<int[]>() ?? throw NullArrayExc(nameof(hihatMem));
+                snareMem = jsonArray[2]?.ToObject<int[]>() ?? throw NullArrayExc(nameof(snareMem)); 
+                kickMem = jsonArray[3]?.ToObject<int[]>() ?? throw NullArrayExc(nameof(kickMem));
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show($"Invalid JSON-file, {ex.Message}", "Error in Array");
+            }
+            DebugNow();
+        }
+        private InvalidOperationException NullArrayExc(string arrayName)
+        {
+            return new InvalidOperationException($"Array '{arrayName}' is null.");
+        }
+
 
         private void btnPlay_Click(object sender, RoutedEventArgs e)
         {
             _dispatcherTimer.Start();
-
-            ////BELANGRIJK
-            ////Bij Timer: gebruik 'Priority.Normal' voor hogere precisie
-            ////BELANGRIJK
-            //int i = 0;
-            //Stopwatch stopwatch = new Stopwatch(); // use higher precision timer
-            //Timer timer = new Timer(msWait);
-            //timer.Elapsed += (sender, e) =>
-            //{
-            //    while (true) // repeat infinitely
-            //    {
-            //        stopwatch.Start(); // start the timer
-            //        if (i < crashMem.Length)
-            //        {
-            //            if (crashMem[i] == 1)
-            //            {
-            //                Thread t = new Thread(() => _crash.Play());
-            //                t.Start();
-            //            }
-            //            if (hihatMem[i] == 1)
-            //            {
-            //                Thread t = new Thread(() => _hihat.Play());
-            //                t.Start();
-            //            }
-            //            if (i < snareMem.Length && snareMem[i] == 1)
-            //            {
-            //                Thread t = new Thread(() => _snare.Play());
-            //                t.Start();
-            //            }
-            //            if (kickMem[i] == 1)
-            //            {
-            //                Thread t = new Thread(() => _kick.Play());
-            //                t.Start();
-            //            }
-            //            i++;
-            //        }
-            //        else
-            //        {
-            //            i = 0;
-            //        }
-            //        stopwatch.Stop(); // stop the timer
-            //        int elapsedMs = (int)stopwatch.Elapsed.TotalMilliseconds; // calculate elapsed time
-            //        int remainingMs = msWait - elapsedMs; // calculate remaining time
-            //        if (remainingMs > 0)
-            //        {
-            //            Thread.Sleep(remainingMs); // wait for the remaining time
-            //        }
-            //        stopwatch.Reset(); // reset the timer
-            //    }
-            //};
-            //timer.Start();
-
-            //loopActive = true;
-            //int i = 0;
-            //Stopwatch stopwatch = new Stopwatch();
-            //MultimediaTimer timer = new MultimediaTimer();
-            //timer.Interval = msWait;
-            //timer.Start();
-            //while (loopActive)
-            //{
-            //    timer.Elapsed += (sender, e) =>
-            //    {
-            //        if (crashMem[i] == 1)
-            //        {
-            //            Task.Run(() => _crash.Play());
-            //        }
-            //        if (hihatMem[i] == 1)
-            //        {
-            //            Task.Run(() => _hihat.Play());
-            //        }
-            //        if (i < snareMem.Length && snareMem[i] == 1)
-            //        {
-            //            Task.Run(() => _snare.Play());
-            //        }
-            //        if (kickMem[i] == 1)
-            //        {
-            //            Task.Run(() => _kick.Play());
-            //        }
-            //        i++;
-            //        if (i >= crashMem.Length)
-            //            i = 0;
-            //    };
-            //}
-            //timer.Stop();
-
-
-
-            //loopActive = true;
-            //int i = 0;
-            //Stopwatch stopwatch = new Stopwatch();
-            //timer.Interval = msWait;
-            //timer.Start();
-            //timer.Elapsed += (sender, e) =>
-            //{
-            //    stopwatch.Start();
-            //    if (i < crashMem.Length)
-            //    {
-            //        Parallel.ForEach(Enumerable.Range(0, 4), (index) =>
-            //        {
-            //            if (crashMem[i] == 1 && index == 0)
-            //            {
-            //                _crash.Play();
-            //            }
-            //            if (hihatMem[i] == 1 && index == 1)
-            //            {
-            //                _hihat.Play();
-            //            }
-            //            if (snareMem[i] == 1 && index == 2)
-            //            {
-            //                _snare.Play();
-            //            }
-            //            if (kickMem[i] == 1 && index == 3)
-            //            {
-            //                _kick.Play();
-            //            }
-            //        });
-            //        i++;
-            //    }
-            //    else
-            //    {
-            //        i = 0;
-            //    }
-            //};
         }
 
         private void btnStop_Click(object sender, RoutedEventArgs e)
         {
             _dispatcherTimer.Stop();
         }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            soundBtnList.Clear();
+
+            // Find all the buttons in the visual tree except for the specific ones
+            FindButtons(this, soundBtnList);
+
+            Array.Clear(crashMem, 0, crashMem.Length);
+            Array.Clear(hihatMem, 0, hihatMem.Length);
+            Array.Clear(snareMem, 0, snareMem.Length);
+            Array.Clear(kickMem, 0, kickMem.Length);
+
+            foreach (Button button in soundBtnList)
+            {
+                button.Background = Brushes.LightGray;
+            }
+
+            for (int i = 0; i < soundBtnList.Count - 1; i += 4)
+            {
+                SetDarkGrayBackground(soundBtnList, i);
+            }
+        }
+
+        private void FindButtons(DependencyObject parent, List<Button> soundBtnList)
+        {
+            int count = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < count; i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(parent, i);
+                //if (child is Button button && button.Name != "btnPlay" && button.Name != "btnStop" && button.Name != "btnClear")
+                if (child is Button button && !antiBtnList.Contains(button))
+                {
+                    soundBtnList.Add(button);
+                }
+                else
+                {
+                    FindButtons(child, soundBtnList);
+                }
+            }
+        }
+
+        private void SetDarkGrayBackground(List<Button> buttonList, int index)
+        {
+            if (index >= 0 && index < buttonList.Count)
+            {
+                Button button = buttonList[index];
+                button.Background = new SolidColorBrush(Color.FromRgb(0xA9, 0xA9, 0xA9));
+            }
+        }
+
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            // Create a SaveFileDialog instance
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "JSON files (*.json)|*.json";
+            saveFileDialog.Title = "Save Arrays to JSON";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string filename = saveFileDialog.FileName;
+                SaveArraysToJson(new object[] { crashMem, hihatMem, snareMem, kickMem }, filename);
+            }
+        }
+
+        static void SaveArraysToJson(object[] arrays, string filename)
+        {
+            // Convert the arrays to JSON format
+            string json = JsonSerializer.Serialize(arrays, new JsonSerializerOptions { WriteIndented = true });
+
+            // Save the JSON to a file
+            File.WriteAllText(filename, json);
+            Console.WriteLine("JSON data saved to file: " + filename);
+        }
+
 
         private void btnCrash_Click(object sender, RoutedEventArgs e)
         {
@@ -270,7 +298,7 @@ namespace DrumMachine_Project_OOP
         private void btnKick_Click(object sender, RoutedEventArgs e)
         {
             var button = (Button)sender;
-            int index = int.Parse(button.Name.Substring(7)) - 1; // extract index from button name
+            int index = int.Parse(button.Name.Substring(7)) - 1; // extract index from button name //is '7', want kick is 4 letters, al de rest 5
 
             if (kickMem[index] == 0)
             {
@@ -286,6 +314,38 @@ namespace DrumMachine_Project_OOP
                 kickMem[index] = 0;
             }
             //Debug.WriteLine(index + "[" + kick[index].ToString() + "]");
+        }
+
+        private void DebugNow()
+        {
+            Debug.Write("crashMem[]: {  ");
+            foreach (var item in crashMem)
+            {
+                Debug.Write(item.ToString());
+                Debug.Write($", ");
+            }
+            Debug.Write("   }\n");
+            Debug.Write("hihatMem[]: {  ");
+            foreach (var item in hihatMem)
+            {
+                Debug.Write(item.ToString());
+                Debug.Write($", ");
+            }
+            Debug.Write("   }\n");
+            Debug.Write("snareMem[]: {  ");
+            foreach (var item in snareMem)
+            {
+                Debug.Write(item.ToString());
+                Debug.Write($", ");
+            }
+            Debug.Write("   }\n");
+            Debug.Write("kickMem[]: {  ");
+            foreach (var item in kickMem)
+            {
+                Debug.Write(item.ToString());
+                Debug.Write($", ");
+            }
+            Debug.Write("   }\n");
         }
     }
 }
